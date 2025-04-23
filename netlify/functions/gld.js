88bfbbcb73796213e530dd18644d70e8375cd248
bomi <mibolee1997@naver.com>
2025-04-23 20:48:13 +0800

    GLD ETF 추가

--- netlify/functions/gld.js
+++ netlify/functions/gld.js
@@ -1,6 +1,8 @@
-exports.handler = async function (event, context) {
-  const fetch = await import('node-fetch'); // 동적 import
-  //require('dotenv').config();
+
+// functions/gld.js
+
+exports.handler = async function () {
+  const fetch = await import('node-fetch'); // ESM 방식
   const apiKey = process.env.FINANCE_API_KEY;
   const symbol = 'GLD';
   const url = `https://yfapi.net/v6/finance/quote?symbols=${symbol}`;
@@ -13,9 +15,11 @@ exports.handler = async function (event, context) {
     });
 
     const data = await response.json();
+    const price = data.quoteResponse.result[0]?.regularMarketPrice;
+
     return {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify({ gld: `$${price}` }),
     };
   } catch (err) {
     return {
