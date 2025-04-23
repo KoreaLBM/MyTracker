e19d61b3d5790bae84025bd454384a426b2e653e
bomi <mibolee1997@naver.com>
2025-04-23 16:27:51 +0800

    국내형 주식 추가

--- netlify/functions/korea-etfs.js
+++ netlify/functions/korea-etfs.js
@@ -13,17 +13,29 @@ exports.handler = async function (event, context) {
 
     const page = await browser.newPage();
 
+    // TIGER 미국S&P500 (A143850)
     await page.goto('https://finance.naver.com/item/main.nhn?code=143850');
     const price1 = await page.$eval('.no_today .blind', el => el.innerText);
 
+    // TIGER 나스닥100 (A379800)
     await page.goto('https://finance.naver.com/item/main.nhn?code=379800');
     const price2 = await page.$eval('.no_today .blind', el => el.innerText);
+
+    // KODEX 미국배당다우존스 (A354500)
+    await page.goto('https://finance.naver.com/item/main.nhn?code=354500');
+    const price3 = await page.$eval('.no_today .blind', el => el.innerText);
+
+    // KODEX 삼성전자채권혼합 (A329200)
+    await page.goto('https://finance.naver.com/item/main.nhn?code=329200');
+    const price4 = await page.$eval('.no_today .blind', el => el.innerText);
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         snp500: price1,
         nasdaq: price2,
+        dividend: price3,
+        bondmix: price4,
       }),
     };
   } catch (err) {
